db9df3adb3ef71135a7c040a4c1d068c1512c06f
OsmanGundogdu54 <OsmanGundogdu@users.noreply.github.com>
2025-04-02 22:18:43 +0300

    yorum satırları silindi

--- Models/Kullanici.cs
+++ Models/Kullanici.cs
@@ -1,28 +1,3 @@
-// using System.ComponentModel.DataAnnotations;
-
-// namespace MakaleSistemi.Models
-// {
-//     public class Kullanici
-//     {
-//         [Key]
-//         public int Id { get; set; }
-
-//         [Required]
-//         public string AdSoyad { get; set; } = string.Empty;
-
-//         [Required, EmailAddress]
-//         public string Email { get; set; } = string.Empty;
-
-//         [Required]
-//         public string Sifre { get; set; } = string.Empty;
-
-//         [Required]
-//         public string Rol { get; set; } = string.Empty;
-
-//         public string? IlgiAlani { get; set; }
-//     }
-// }
-
 using System.ComponentModel.DataAnnotations;
 
 namespace MakaleSistemi.Models
